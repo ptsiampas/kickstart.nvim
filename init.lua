4c17303bfa54d715d969d0693938be4d19949727
peter@tsiampas.com <peter@tsiampas.com>
2024-03-28 14:11:29 +1100

    Allows for dictionary to be loaded for txt and md files.

--- init.lua
+++ init.lua
@@ -204,6 +204,15 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
+-- Automatically load up the dictionary when its a text or markdown file
+vim.api.nvim_create_autocmd({ 'BufRead', 'BufNewFile' }, {
+  pattern = { '*.md', '*.txt' },
+  callback = function()
+    vim.opt_local.spell = true
+    vim.opt_local.spelllang = 'en_gb'
+  end,
+})
+
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
